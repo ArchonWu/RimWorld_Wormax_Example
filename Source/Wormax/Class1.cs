b251323296dc54e78ee09d5e484231123890e3db
ArchonWu <archonuwu@gmail.com>
2025-05-16 03:30:22 -0700

    IngestionOutcomeDoer_RemoveHediff class

--- Source/Wormax/Class1.cs
+++ Source/Wormax/Class1.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RimWorld;
+using Verse;
 
 namespace Wormax
 {
-    public class Class1
+    public class IngestionOutcomeDoer_RemoveHediff : IngestionOutcomeDoer
     {
+        public HediffDef removeThisThing;
+
+        protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested, int ingestedCount)
+        {
+            // targetHediff would be gutworm in our case
+            var targetHediff = pawn.health.hediffSet.GetFirstHediffOfDef(removeThisThing);
+            if (targetHediff != null)
+            {
+                pawn.health.hediffSet.hediffs.Remove(targetHediff);
+            }
+        }
     }
 }
